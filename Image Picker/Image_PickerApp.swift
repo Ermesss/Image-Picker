013119412fed37f8c563afe9848125f2d08269ee
Ermanno Fissore <ermannofissore@gmail.com>
2022-09-28 18:40:40 +0200

    Initial Commit

---
+++ Image Picker/Image_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_PickerApp.swift
+//  Image Picker
+//
+//  Created by Ermanno Fissore on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Image_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
